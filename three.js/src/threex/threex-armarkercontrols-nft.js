f4e2d92c00267cfa974f4bd4c1100233d0a04e1c
Walter Perdan <github@kalwaltart.it>
2020-03-02 21:04:41 +0100

    injecting the Worker with a Blob

--- three.js/src/threex/threex-armarkercontrols-nft.js
+++ three.js/src/threex/threex-armarkercontrols-nft.js
@@ -272,8 +272,12 @@ ARjs.MarkerControls.prototype._initArtoolkit = function () {
 
     function handleNFT(descriptorsUrl, arController) {
         // create a Worker to handle loading of NFT marker and tracking of it
-
-        var worker = new Worker(THREEx.ArToolkitContext.baseURL + 'vendor/jsartoolkit5/js/artoolkit.worker.js');
+        var workerBlob = new Blob(
+          [workerRunner.toString().replace(/^function .+\{?|\}$/g, '')],
+          { type:'text/javascript' }
+        );
+        var workerBlobUrl = URL.createObjectURL(workerBlob);
+        var worker = new Worker(workerBlobUrl);
 
         window.addEventListener('arjs-video-loaded', function (ev) {
             var video = ev.detail.component;
@@ -360,6 +364,85 @@ ARjs.MarkerControls.prototype._initArtoolkit = function () {
         })
 
 
+
+    }
+
+    function workerRunner() {
+      if ('function' === typeof importScripts) {
+          importScripts('./../../three.js/vendor/jsartoolkit5/build/artoolkit-nft.min.js');
+
+          self.onmessage = function (e) {
+              var msg = e.data;
+              switch (msg.type) {
+                  case "init": {
+                      load(msg);
+                      return;
+                  }
+                  case "process": {
+                      next = msg.imagedata;
+                      process();
+                      return;
+                  }
+              }
+          };
+
+          var next = null;
+
+          var ar = null;
+          var markerResult = null;
+
+          function load(msg) {
+              var path = '../../';
+
+              var onLoad = function () {
+                  ar = new ARController(msg.pw, msg.ph, param);
+                  var cameraMatrix = ar.getCameraMatrix();
+
+                  // after the ARController is set up, we load the NFT Marker
+                  ar.loadNFTMarker(path + msg.marker, function (markerId) {
+                      ar.trackNFTMarkerId(markerId);
+                      postMessage({ type: 'endLoading' })
+                  }, function (err) {
+                      console.log('Error in loading marker on Worker', err)
+                  });
+
+                  // ...and we listen for event when marker has been found from camera
+                  ar.addEventListener('getNFTMarker', function (ev) {
+                      // let AR.js know that a NFT marker has been found, with its matrix for positioning
+                      markerResult = {
+                          type: 'found',
+                          matrix: JSON.stringify(ev.data.matrix),
+                      };
+                  });
+
+                  postMessage({type: "loaded", proj: JSON.stringify(cameraMatrix)});
+              };
+
+              var onError = function (error) {
+                  console.error(error);
+              };
+
+              // we cannot pass the entire ARController, so we re-create one inside the Worker, starting from camera_param
+              var param = new ARCameraParam(path + msg.param, onLoad, onError);
+          }
+
+          function process() {
+              markerResult = null;
+
+              if (ar && ar.process) {
+                  ar.process(next);
+              }
+
+              if (markerResult) {
+                  postMessage(markerResult);
+              } else {
+                  postMessage({
+                      type: "not found",
+                  });
+              }
+              next = null;
+          }
+        }
 
     }
 
