4889b4cf2f633395078e6f698d833f2e9b99db37
Michaël Dierick <michael@dierick.io>
2020-04-07 20:58:28 +0200

    Entrypoints

---
+++ aframe/src/index-nft.js
@@ -0,0 +1,10 @@
+// Components
+import './component-anchor-nft.js';
+import './component-hit-testing.js';
+
+// Location-based components
+import './location-based/gps-camers.js';
+import './location-based/gps-entity-place.js';
+
+// System
+import './system-arjs.js';
