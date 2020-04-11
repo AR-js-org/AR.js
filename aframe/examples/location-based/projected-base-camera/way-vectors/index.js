9e83308f6cc4a80b8d1a5b1b132cc13b47665a29
Nick Whitelegg <nickw4426@gmail.com>
2020-04-11 18:37:20 +0100

    minor cleanup

--- aframe/examples/location-based/projected-base-camera/way-vectors/index.js
+++ aframe/examples/location-based/projected-base-camera/way-vectors/index.js
@@ -1,10 +1,10 @@
 window.onload = () => {
 
-    let gettingData = false, osmElement = document.getElementById('osmElement');
+    const osmElement = document.getElementById('osmElement');
 
     document.getElementById('status').innerHTML = 'Loading OSM data...';
     
-    let gpsProjCamera = document.querySelector('a-camera').components['gps-projected-camera'];
+    const gpsProjCamera = document.querySelector('a-camera').components['gps-projected-camera'];
 
     osmElement.setAttribute('osm', {
         longitude: gpsProjCamera.data.simulateLongitude,
@@ -13,6 +13,5 @@ window.onload = () => {
 
     osmElement.addEventListener('vector-ways-loaded', e=> {
         document.getElementById('status').innerHTML = '';
-        gettingData = false;
     });
 }
