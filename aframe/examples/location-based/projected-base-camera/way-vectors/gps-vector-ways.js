cb9dffbadfbfd047fb55de3f54f624ec01eca51f
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-04-17 11:04:58 +0200

    indentation on code, fixed tupo (bundle=index.js)

--- aframe/examples/location-based/projected-base-camera/way-vectors/gps-vector-ways.js
+++ aframe/examples/location-based/projected-base-camera/way-vectors/gps-vector-ways.js
@@ -5,13 +5,13 @@ AFRAME.registerComponent('gps-vector-ways', {
 
         this.vectorWaysListener = (ev) => {
             var camera = document.querySelector('[gps-projected-camera]');
-            if(!camera.components['gps-projected-camera']) {
+            if (!camera.components['gps-projected-camera']) {
                 console.error('gps-projected-camera not initialised');
             } else {
-                if(!this.originSphMerc) {
+                if (!this.originSphMerc) {
                     this.originSphMerc = camera.components['gps-projected-camera'].originCoordsProjected;
                 }
-                ev.detail.objectIds.forEach ( k=> {
+                ev.detail.objectIds.forEach(k => {
                     this.el.object3DMap[k].geometry.translate(-this.originSphMerc[0], 0, this.originSphMerc[1]);
                 });
             }
