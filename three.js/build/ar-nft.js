716e8dd438de8c46b6bb5efbcdd47b1d1d07c060
Walter Perdan <github@kalwaltart.it>
2020-04-30 17:21:20 +0200

    new listener for end loading of NFT marker

--- three.js/build/ar-nft.js
+++ three.js/build/ar-nft.js
@@ -660,6 +660,8 @@ ARjs.MarkerControls.prototype._initArtoolkit = function () {
                     if (loader) {
                         loader.remove();
                     }
+                    var endLoadingEvent = new Event('arjs-nft-loaded');
+                    window.dispatchEvent(endLoadingEvent);
                 }
 
                 if (ev && ev.data && ev.data.type === 'loaded') {
