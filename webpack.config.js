83e030d12b610417280de2037486f3608ca931eb
Michaël Dierick <michael@dierick.io>
2020-05-17 11:39:05 +0200

    Configure nft-build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 var path = require('path');
 
-module.exports = env => {
-  const mode = env.production ? 'production' : 'development';
-  const devtool = env.production ? false : 'inline-source-map';
-  console.log(`${mode} build`);
+module.exports = (env, argv) => {
+  let devtool = false;
+  if (argv.mode === 'development') {
+    devtool = 'inline-source-map';
+  }
+  console.log(`${argv.mode} build`);
+  const externals = {
+    aframe: {
+      commonjs: 'aframe',
+      commonjs2: 'aframe',
+      amd: 'aframe',
+      root: 'AFRAME' // indicates global variable
+    },
+    three: {
+      commonjs: 'three',
+      commonjs2: 'three',
+      amd: 'three',
+      root: 'THREE' // indicates global variable
+    }
+  };
 
   return [{
     name: 'default',
-    mode,
     devtool,
     entry: './aframe/src/index.js',
     output: {
@@ -19,40 +34,27 @@ module.exports = env => {
     },
     resolve: {
       alias: {
-        jsartoolkit: 'artoolkit5-js'
+        jsartoolkit: 'artoolkit5-js',
       }
     },
-    externals: {
-      aframe: {
-        commonjs: 'aframe',
-        commonjs2: 'aframe',
-        amd: 'aframe',
-        root: 'AFRAME' // indicates global variable
-      },
-      three: {
-        commonjs: 'three',
-        commonjs2: 'three',
-        amd: 'three',
-        root: 'THREE' // indicates global variable
+    externals
+  },
+  {
+    name: 'nft',
+    devtool,
+    entry: './aframe/src/index-nft.js',
+    output: {
+      library: 'ARjs',
+      path: path.resolve(__dirname, 'aframe/build'),
+      filename: 'aframe-ar-nft.js',
+      libraryTarget: 'umd',
+      globalObject: 'this'
+    },
+    resolve: {
+      alias: {
+        jsartoolkit: 'artoolkit5-js',
       }
-    }
-    // {
-    //   name: 'nft',
-    //   mode,
-    //   devtool,
-    //   entry: './aframe/src/index-nft.js',
-    //   output: {
-    //     library: 'ARjs',
-    //     path: path.resolve(__dirname, 'aframe/build'),
-    //     filename: 'aframe-ar-nft.js',
-    //     libraryTarget: 'umd',
-    //     globalObject: 'this'
-    //   },
-    //   resolve: {
-    //     alias: {
-    //       $jsartoolkit: 'artoolkit5-js'
-    //     }
-    //   }
-    // }
+    },
+    externals
   }];
 };
