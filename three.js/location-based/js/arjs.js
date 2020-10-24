105b6a6b5617740d70cc7627d193421c040d8ff1
Nick Whitelegg <nickw4426@gmail.com>
2020-10-24 13:50:48 +0200

    Start on three.js location-based

---
+++ three.js/location-based/js/arjs.js
@@ -0,0 +1,9 @@
+import { LocationBased } from './location-based.js';
+import { WebcamRenderer } from './webcam-renderer.js';
+import { ArjsDeviceOrientationControls as DeviceOrientationControls } from './arjs-device-orientation-controls.js';
+
+export {
+    LocationBased,
+    WebcamRenderer,
+    DeviceOrientationControls
+};
