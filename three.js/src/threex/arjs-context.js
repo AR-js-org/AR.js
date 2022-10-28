b3b7c316b232b14e71f8b8b9c8bf4ee38d264f01
kalwalt <info@kalwaltart.it>
2022-10-28 23:57:05 +0200

    upgrading artoolkit5-js with more MatrixCodeTypes

--- three.js/src/threex/arjs-context.js
+++ three.js/src/threex/arjs-context.js
@@ -289,6 +289,8 @@ Context.prototype._initArtoolkit = function (onCompleted) {
       "4x4": arController.artoolkit.AR_MATRIX_CODE_4x4,
       "4x4_BCH_13_9_3": arController.artoolkit.AR_MATRIX_CODE_4x4_BCH_13_9_3,
       "4x4_BCH_13_5_5": arController.artoolkit.AR_MATRIX_CODE_4x4_BCH_13_5_5,
+      "5x5_BCH_22_12_5": arController.artoolkit.AR_MATRIX_CODE_5x5_BCH_22_12_5,
+      "5x5_BCH_22_7_7": arController.artoolkit.AR_MATRIX_CODE_5x5_BCH_22_7_7,
       "5x5": arController.artoolkit.AR_MATRIX_CODE_5x5,
       "6x6": arController.artoolkit.AR_MATRIX_CODE_6x6,
     };
