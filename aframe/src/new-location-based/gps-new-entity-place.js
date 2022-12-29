d5fd88afe6bfa726c7c8cc17727a2e7a6afb8210
Nick Whitelegg <nickw4426@gmail.com>
2022-12-29 14:11:41 +0000

    Add distance property to gps-new-entity-place, to represent distance from GPS position

--- aframe/src/new-location-based/gps-new-entity-place.js
+++ aframe/src/new-location-based/gps-new-entity-place.js
@@ -21,6 +21,10 @@ AFRAME.registerComponent("gps-new-entity-place", {
       return;
     }
     this._cameraGps = camera.components["gps-new-camera"];
+
+    this.el.addEventListener("gps-camera-update-position", (e) => {
+      this.distance = this._haversineDist(e.detail.position, this.data);
+    });
   },
 
   update: function () {
@@ -33,5 +37,22 @@ AFRAME.registerComponent("gps-new-entity-place", {
       this.el.object3D.position.y,
       projCoords[1]
     );
+  },
+  /**
+   * Calculate haversine distance between two lat/lon pairs.
+   *
+   * Taken from gps-camera
+   */
+  _haversineDist: function (src, dest) {
+    const dlongitude = THREE.Math.degToRad(dest.longitude - src.longitude);
+    const dlatitude = THREE.Math.degToRad(dest.latitude - src.latitude);
+
+    const a =
+      Math.sin(dlatitude / 2) * Math.sin(dlatitude / 2) +
+      Math.cos(THREE.Math.degToRad(src.latitude)) *
+        Math.cos(THREE.Math.degToRad(dest.latitude)) *
+        (Math.sin(dlongitude / 2) * Math.sin(dlongitude / 2));
+    const angle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return angle * 6371000;
   },
 });
